97fd130a8227608f02e85bca85b94c5ac3781900
Łukasz Gałka <lukasz.galka@intive.com>
2022-01-12 09:53:34 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Patronage 22 Szczecin JS - Retro Board
+
+## Dependencies
+
+```
+npm i
+```
+
+## Local development
+
+```
+npm run dev
+```
---
+++ pages/index.page.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Retro Board - Szczecin JS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          PATRONAGE22 SZCZECIN JS
+        </h1>
+      </main>
+    </div>
+  )
+}
