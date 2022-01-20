2ddeea8b6594ee8d7517677dd3a5576fd7437fa2
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2022-01-20 17:20:36 +0100

    feat: P2022-148 Add Material Icons

--- pages/index.page.js
+++ pages/index.page.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Icon from '../components/Icon'
 
 export default function Home () {
   return (
@@ -6,12 +7,19 @@ export default function Home () {
       <Head>
         <title>Retro Board - Szczecin JS</title>
         <link rel='icon' href='/favicon.ico' />
+        <link
+          href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          rel='stylesheet'
+        />
       </Head>
 
       <main>
         <h1>
           PATRONAGE22 SZCZECIN JS
         </h1>
+        <p>
+          Material Icons <Icon name='tag_faces' styleClass='' />
+        </p>
       </main>
     </div>
   )
