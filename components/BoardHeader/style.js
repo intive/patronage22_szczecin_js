2ddf54c7f8851a2eeba47804b364578974a3e2db
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2022-02-07 13:51:06 +0100

    feat: P2022-150 Add board header component

---
+++ components/BoardHeader/style.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  padding: 1.5rem 5%;
+  background: #E1E1E1;
+  gap: 1rem;
+
+  @media (min-width: 1440px) {
+    padding: 1.5rem 2.5rem;
+  }
+`
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  height: fit-content; 
+`
+export const Title = styled.h1`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 2.25rem;
+  line-height: 2.5rem;
+  color: #515151;
+`
+export const StyledLink = styled.a`
+  text-decoration: none;
+  width: fit-content;
+  font-style: normal;
+  font-weight: normal;
+  font-size: .75rem;
+  line-height: 1rem;
+  color: #626262;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import { Wrapper } from './style'
-import Header from '../Header/Header'
 import { useTranslation } from 'next-i18next'
+import Header from '../Header/Header'
+import BoardHeader from '../BoardHeader/BoardHeader'
 
 export default function Layout (props) {
   const { t } = useTranslation('common')
@@ -8,6 +9,7 @@ export default function Layout (props) {
   return (
     <>
       <Header title={t('headerTitle')} />
+      <BoardHeader returnLinkText={t('boardHeader.returnLinkText')} buttonText={t('boardHeader.buttonText')} title='Board name 1' />
       <Wrapper>{props.children}</Wrapper>
     </>
   )
