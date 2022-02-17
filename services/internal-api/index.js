8839ed25b916a47957475484678e349c215bc79c
Alex Simkovich <oleksandr.simkovich@intive.com>
2022-02-17 17:15:01 +0100

    feat(axios): Axios configuration

--- services/internal-api/index.js
+++ services/internal-api/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const axiosInstance = axios.create()
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
+  responseType: 'json',
+  withCredentials: true
+})
 
 export const getBoards = async (...args) => {
   return axiosInstance.get('/api/boards', { params: Object.assign({}, ...args) })
