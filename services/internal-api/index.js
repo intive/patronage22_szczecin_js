a571b70ae7261d910383dac84126488c403a6c3e
Alex Simkovich <oleksandr.simkovich@intive.com>
2022-02-18 14:12:27 +0100

    fix(axios): BaseURL fix

--- services/internal-api/index.js
+++ services/internal-api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000',
+  baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || 'http://localhost:3000',
   responseType: 'json',
   withCredentials: true
 })
