58ec1927c3f013805060cc45ebe33a9e248e9466
Łukasz Gałka <lukasz.galka@intive.com>
2022-02-19 10:03:05 +0100

    fix: Set proper base url

--- services/internal-api/index.js
+++ services/internal-api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000',
   responseType: 'json',
   withCredentials: true
 })
