6f47b02e915a931547e37431fd7510af6bb457fb
Konrad <stawarzkonrad@gmail.com>
2022-02-24 23:38:27 +0100

    fix: P2022-1247 Fix buttons visibility for mobile

--- pages/index.page.js
+++ pages/index.page.js
@@ -43,7 +43,7 @@ export async function getServerSideProps (ctx) {
   return {
     props: {
       tilesList: resData.data,
-      ...(await serverSideTranslations(ctx.locale, ['common']))
+      ...(await serverSideTranslations(ctx.locale, ['common', 'modals']))
     }
   }
 }
