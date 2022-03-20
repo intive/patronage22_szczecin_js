4c8f632729d819a90660aaf66bd6d3767820b68f
Anna Sawicka-Ziółkowska <aniasawicka@gmail.com>
2022-03-20 13:22:06 +0100

    feat: P2022-1620 Remove card api

--- pages/api/boards/[id]/columns/[columnId]/cards/[cardId]/index.test.js
+++ pages/api/boards/[id]/columns/[columnId]/cards/[cardId]/index.test.js
@@ -21,9 +21,9 @@ get.mockResolvedValue(snapshot)
 
 describe('api/boards/[id]/columns/[columnId]/cards/[cardId]', () => {
   it('should return status code 405 if the method is not allowed', async () => {
-    const id = 111
-    const columnId = 222
-    const cardId = 333
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
     const { req, res } = createMocks({
       method: 'GET',
       url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`
@@ -32,7 +32,7 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId]', () => {
     await handler(req, res)
 
     expect(res._getStatusCode()).toEqual(405)
-    expect(res._getHeaders()).toEqual({ allow: ['PATCH'] })
+    expect(res._getHeaders()).toEqual({ allow: ['PATCH', 'DELETE'] })
   })
 })
 
@@ -43,9 +43,9 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId] patch', () => {
       text: 'Card 1'
     })
 
-    const id = 111
-    const columnId = 222
-    const cardId = 333
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
     const { req, res } = createMocks({
       method: 'PATCH',
       url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
@@ -69,9 +69,9 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId] patch', () => {
     snapshot.exists.mockReturnValue(false)
     snapshot.val.mockResolvedValue()
 
-    const id = 111
-    const columnId = 222
-    const cardId = 333
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
     const { req, res } = createMocks({
       method: 'PATCH',
       url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
@@ -97,9 +97,9 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId] patch', () => {
       text: 'Card 1'
     })
 
-    const id = 111
-    const columnId = 222
-    const cardId = 333
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
     const { req, res } = createMocks({
       method: 'PATCH',
       url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
@@ -138,9 +138,9 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId] patch', () => {
       text: 'Card 1'
     })
 
-    const id = 111
-    const columnId = 222
-    const cardId = 333
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
     const { req, res } = createMocks({
       method: 'PATCH',
       url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
@@ -179,9 +179,9 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId] patch', () => {
       text: 'Card 1'
     })
 
-    const id = 111
-    const columnId = 222
-    const cardId = 333
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
     const { req, res } = createMocks({
       method: 'PATCH',
       url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
@@ -212,5 +212,55 @@ describe('api/boards/[id]/columns/[columnId]/cards/[cardId] patch', () => {
         }
       ]
     })
+  })
+})
+
+describe('api/boards/[id]/columns/[columnId]/cards/[cardId] delete', () => {
+  it('should return status code 204 on success deletion', async () => {
+    snapshot.exists.mockReturnValue(true)
+    snapshot.val.mockResolvedValue({
+      text: 'Card 1'
+    })
+
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
+    const { req, res } = createMocks({
+      method: 'DELETE',
+      url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
+      query: {
+        id: id,
+        columnId: columnId,
+        cardId: cardId
+      }
+    })
+
+    await handler(req, res)
+
+    expect(res._getStatusCode()).toEqual(204)
+    expect(res._getData()).toEqual('')
+  })
+
+  it('should return status code 404 when column was not found', async () => {
+    snapshot.exists.mockResolvedValue(false)
+    snapshot.val.mockResolvedValue()
+
+    const id = '01FZ35K7JCSM2DK4G9ZESG4DGV'
+    const columnId = '01FZ35NJG9GYA6YY80X25WE7ZQ'
+    const cardId = '01FZ3KAPHMK6WCB7322EC8QV5N'
+    const { req, res } = createMocks({
+      method: 'DELETE',
+      url: `/api/boards/${id}/columns/${columnId}/cards/${cardId}`,
+      query: {
+        id: id,
+        columnId: columnId,
+        cardId: cardId
+      }
+    })
+
+    await handler(req, res)
+
+    expect(res._getStatusCode()).toEqual(404)
+    expect(res._getJSONData()).toEqual({ message: `Card with id ${cardId} was not found` })
   })
 })
