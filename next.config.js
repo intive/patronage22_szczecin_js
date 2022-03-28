ac856ae397c68e6017b009d0f8bf10385342485c
Alex Simkovich <oleksandr.simkovich@intive.com>
2022-03-28 08:42:27 +0200

    feat: Enable CORS

--- next.config.js
+++ next.config.js
@@ -8,5 +8,18 @@ module.exports = {
   },
   publicRuntimeConfig: {
     branchName: process.env.VERCEL_GIT_COMMIT_REF
+  },
+  async headers () {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+        ]
+      }
+    ]
   }
 }
