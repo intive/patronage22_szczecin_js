7a8d15247c8b409cd70818968aec008fe60139c0
Łukasz Gałka <lukasz.galka@intive.com>
2022-03-31 11:17:09 +0200

    fix: Set proper url for API on prod

--- services/internal-api/index.js
+++ services/internal-api/index.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
+const getBaseURL = () => {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') return 'https://patronage22-szczecin-js.vercel.app'
+  else if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  else return 'http://localhost:3000'
+}
+
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000',
+  baseURL: getBaseURL(),
   responseType: 'json',
   withCredentials: true
 })
